1f533d5444655bd9c790045c1b24803412165f82
Erice Marial <115804909+ericemarial@users.noreply.github.com>
2023-04-05 21:05:30 +0800

    Add files via upload

--- COMPROG2/1-3.c
+++ COMPROG2/1-3.c
@@ -1,24 +1,33 @@
+// Create a C program that will search for an integer value from a list of n integer values
+// and count the number of occurrences of that number.
 #include<stdio.h>
+#include<conio.h>
 
 main () {
     int marr[100], count, i, srch, occr=0;
 
+    printf("--     NUMBER OCCURRENCE DETERMINER     --\n");
     printf("How many integers do you want in a list? ");
     scanf("%d", &count); // number of elements stored in 'count' variable
 
     for (i = 0; i < count; i++) {  // for-loop in getting the values 
-        printf("Element %d value = ", i);
+        printf("Value of element number %d  = ", i);
         scanf("%d", &marr[i]); // every element is stored in main array variable called 'marr'
     }
 
-    printf("What is the number to be searched? ");
+    printf("\nWhat is the number to be searched? ");
     scanf("%d", &srch); // number to be searched is stored in 'srch' variable
 
     for (i = 0; i < count; i++) { // for-loop in comparing the number to be searched and the main array
         if (marr[i] == srch) {
-            occr++; // if array and srch is same, then occurrence is plus one (+1)
+            occr++; // if array and srch is same, then occr is plus one (+1)
         }
     }
 
-    printf("There are %d occurence/s of the number %d", occr, srch); // displaying output
+    if (occr == 1) {  // displaying output
+        printf("\nThere is %d occurrence of the number %d.", occr, srch); 
+    } else {
+        printf("\nThere are %d occurrences of the number %d.", occr, srch);
+    }
+    getch(); 
 }
